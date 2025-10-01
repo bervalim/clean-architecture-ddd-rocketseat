6eddda1974679a868820958e54fc7b275158b99d
bervalim <bernardogvalim@gmail.com>
2025-09-30 22:20:51 -0300

    abstract entity

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,10 +1,26 @@
 import { Entity } from "../core/entities/entity"
+import { UniqueEntityId } from "../core/entities/unique-entity-id"
+import { Optional } from "../core/types/optional"
 
 interface AnswerProps {
   content: string
-  authorId: string
-  questionId: string
+  authorId: UniqueEntityId
+  questionId: UniqueEntityId
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
+  get content(){
+    return this.props.content
+  }
+
+  static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityId ) {
+      const question = new Answer({
+        ...props,
+        createdAt: new Date()
+      },id)
+  
+      return question
+    }
 }
